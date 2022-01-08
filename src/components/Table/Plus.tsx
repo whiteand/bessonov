29ce0486f5b878965014ad71c04ba9c6feb72d24
whiteand <andrewbeletskiy@gmail.com>
2022-01-08 21:07:40 +0200

    added appropriate tooltips

--- src/components/Table/Plus.tsx
+++ src/components/Table/Plus.tsx
@@ -1,44 +1,94 @@
-import { createEffect, createSignal, JSX, onCleanup, Show } from "solid-js";
-import s from "./Table.module.scss";
-import ContactButtonLink from "../ContactButtonLink/ContactButtonLink";
-import triangle from "./triangle.svg";
-import circle from "./circle.svg";
+import {
+  createEffect,
+  createRenderEffect,
+  createSignal,
+  JSX,
+  onCleanup,
+  Show,
+} from "solid-js";
+import usePopper from "solid-popper";
 import { createIsMobile } from "../../packages/createIsMobile";
-import { getZIndex } from "../../utils/getZIndex";
+import preventOverflow from "@popperjs/core/lib/modifiers/preventOverflow.js";
+import ContactButtonLink from "../ContactButtonLink/ContactButtonLink";
+import circle from "./circle.svg";
+import s from "./Table.module.scss";
+import triangle from "./triangle.svg";
+import { Modifier } from "@popperjs/core";
 
 export function Plus(): JSX.Element {
   const isMobile = createIsMobile();
-  const [visible, setVisible] = createSignal(false);
-  createEffect(() => {
-    if (!visible()) return;
-    function handler() {
-      setVisible(false);
-    }
-    window.addEventListener("click", handler);
-    onCleanup(() => {
-      window.removeEventListener("click", handler);
-    });
-  });
+
   return (
     <Show when={isMobile()} fallback={<>+</>}>
+      <PlusWithButton />
+    </Show>
+  );
+}
+const hideOnNotTop = (hide: () => void): Modifier<"hideOnNotTop", unknown> => ({
+  name: "hideOnNotTop",
+  enabled: true,
+  phase: "main",
+  fn(params) {
+    if (params.state.placement.indexOf("top") < 0) {
+      hide();
+    }
+  },
+});
+
+function PlusWithButton() {
+  const [anchor, setAnchor] = createSignal<HTMLElement>();
+  const [popper, setPopper] = createSignal<HTMLElement>();
+  const [visible, setVisible] = createSignal(false);
+
+  const hide = () => setVisible(false);
+
+  const res = usePopper(anchor, popper, {
+    placement: "top-start",
+    strategy: "absolute",
+    modifiers: [
+      hideOnNotTop(hide),
+      preventOverflow,
+      {
+        name: "offset",
+        options: {
+          offset: [0, 23],
+        },
+      },
+    ],
+  });
+
+  createEffect(() => {
+    if (!visible()) return;
+    const popper = res();
+    if (!popper) return;
+    if (popper.state.placement.indexOf("top") !== 0 && visible()) {
+      setVisible(false);
+    }
+  });
+
+  createRenderEffect(() => {
+    visible();
+    res()?.forceUpdate();
+  });
+
+  return (
+    <>
       <button
+        ref={setAnchor}
         type="button"
         class={s.plus}
-        classList={{
-          [s.popoverVisible]: visible(),
-        }}
-        onClick={[setVisible, true]}
+        onClick={() => setVisible((visible) => !visible)}
       >
         +
-        <div class={s.popover} style={{ "z-index": getZIndex("popover") }}>
-          <div class={s.text}>Со значком «+» указана минимальная стоимость</div>
-          <ContactButtonLink preset="small">
-            Узнать точную цену
-          </ContactButtonLink>
-          <img width="20" class={s.triangle} src={triangle} />
+      </button>
+      <div ref={setPopper} class={s.popover} data-show={visible()}>
+        <div class={s.text}>Со значком «+» указана минимальная стоимость</div>
+        <ContactButtonLink preset="small">Узнать точную цену</ContactButtonLink>
+        <div data-popper-arrow>
+          <img width="20" class={s.triangle} src={triangle} data-popper-arrow />
           <img class={s.circle} src={circle} />
         </div>
-      </button>
-    </Show>
+      </div>
+    </>
   );
 }
