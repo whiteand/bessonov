2aa48e4ea9ff2ddc162154d5d075ffaf73d05f96
whiteand <andrewbeletskiy@gmail.com>
2022-01-09 21:49:02 +0200

    removed dead code

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,12 +1,7 @@
-import { For, Match, Switch } from "solid-js";
 import { Dynamic } from "solid-js/web";
-import services from "../../config/services.json";
 import { createIsMobile } from "../../packages/createIsMobile";
-import Table from "../Table/Table";
-import { TColumn, TData } from "../Table/types";
 import DesktopServices from "./DesktopServices";
 import MobileServices from "./MobileServices";
-import s from "./Services.module.scss";
 
 export default function Services() {
   const isMobile = createIsMobile();
