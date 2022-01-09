e6106d1c73fb382fa337ece7fa24b03aa1886e65
whiteand <andrewbeletskiy@gmail.com>
2022-01-09 21:48:14 +0200

    added appropriate rendering of sup text

--- src/components/Table/TextCell.tsx
+++ src/components/Table/TextCell.tsx
@@ -1,11 +1,47 @@
-import { JSX } from "solid-js";
+import { createMemo, For, JSX, Match, Switch } from "solid-js";
 import s from "./Table.module.scss";
+
+interface ITextPart {
+  type: "text" | "sup";
+  text: string;
+}
 
 export function TextCell(props: {
   text: string;
   align?: "left" | "right" | "middle";
   columns: number;
 }): JSX.Element {
+  const textParts = createMemo((): ITextPart[] => {
+    const text = props.text;
+    if (text.indexOf("^") < 0) return [{ type: "text", text }];
+
+    const parts: ITextPart[] = [];
+    let mode: "text" | "sup" = "text";
+    for (let i = 0; i < text.length; i++) {
+      const ch = text[i];
+      if (ch === "^") {
+        mode = "sup";
+        continue;
+      }
+      if (parts.length <= 0) {
+        parts.push({
+          type: mode,
+          text: ch,
+        });
+        continue;
+      }
+      const last = parts[parts.length - 1];
+      if (last.type === mode) {
+        last.text += ch;
+        continue;
+      }
+      parts.push({
+        type: mode,
+        text: ch,
+      });
+    }
+    return parts;
+  });
   return (
     <div
       class={s.cell}
@@ -17,7 +53,15 @@ export function TextCell(props: {
         [s.right]: props.align === "right",
       }}
     >
-      {props.text}
+      <For each={textParts()}>
+        {(part) => (
+          <Switch fallback={<>{part.text}</>}>
+            <Match when={part.type === "sup"}>
+              <sup>{part.text}</sup>
+            </Match>
+          </Switch>
+        )}
+      </For>
     </div>
   );
 }
