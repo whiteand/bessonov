ccab05c421fcd3b3ac1405d3ce89aa14afd173c9
whiteand <andrewbeletskiy@gmail.com>
2022-01-11 00:56:37 +0200

    added line

--- src/components/Services/PlusDescription.tsx
+++ src/components/Services/PlusDescription.tsx
@@ -3,6 +3,9 @@ import s from "./PlusDescription.module.scss";
 export default function PlusDescription() {
   return (
     <div class={s.wrapper}>
+      <svg viewBox="0 0 40 33" class={s.line}>
+        <path d="M40,0L0,33" stroke="white" />
+      </svg>
       <p>Со значком «+» указана минимальная стоимость.</p>
       <p>Узнать точную стоимость можно во время консультации.</p>
     </div>
