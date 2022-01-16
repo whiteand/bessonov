c38d079c36f03ec29b2c9aede8fb0ef74b39585c
whiteand <andrewbeletskiy@gmail.com>
2022-01-16 14:18:44 +0200

    removed table editor route

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { Route, Routes } from "solid-app-router";
 import { Component, lazy } from "solid-js";
 
 const Homepage = lazy(() => import("./pages/Homepage"));
-const CreateTable = lazy(() => import("./pages/CreateTable"));
+// const CreateTable = lazy(() => import("./pages/CreateTable"));
 
 const App: Component = () => {
   return (
     <>
       <Routes>
-        <Route path="/create-table" element={<CreateTable />} />
+        {/* <Route path="/create-table" element={<CreateTable />} /> */}
         <Route path="/" element={<Homepage />} />
         <Route path="/:all" element={<Homepage />} />
       </Routes>
