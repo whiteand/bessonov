8155e216ab575de5951aa04cb48d421aac40b1eb
whiteand <andrewbeletskiy@gmail.com>
2022-01-30 13:34:01 +0200

    rewrote popper without popper js

--- src/components/Table/Plus.tsx
+++ src/components/Table/Plus.tsx
@@ -1,9 +1,5 @@
-import { Modifier } from "@popperjs/core";
-import preventOverflow from "@popperjs/core/lib/modifiers/preventOverflow.js";
 import { createEffect, createSignal, JSX, onCleanup, Show } from "solid-js";
-import usePopper from "solid-popper";
 import { createIsMobile } from "../../packages/createIsMobile";
-import { isClickOutside } from "../../packages/isClickOutside";
 import circle from "./circle.svg";
 import s from "./Table.module.scss";
 import triangle from "./triangle.svg";
@@ -17,66 +13,21 @@ export function Plus(props: { withTooltip?: boolean }): JSX.Element {
     </Show>
   );
 }
-const hideOnNotTop = (hide: () => void): Modifier<"hideOnNotTop", unknown> => ({
-  name: "hideOnNotTop",
-  enabled: true,
-  phase: "main",
-  fn(params) {
-    if (params.state.placement.indexOf("top") < 0) {
-      hide();
-    }
-  },
-});
 
 function PlusWithButton() {
-  const [anchor, setAnchor] = createSignal<HTMLElement>();
-  const [popper, setPopper] = createSignal<HTMLElement>();
-  const [visible, setVisible] = createSignal(false);
-
-  const hide = () => setVisible(false);
-
-  const res = usePopper(anchor, popper, {
-    placement: "top-start",
-    strategy: "absolute",
-    modifiers: [
-      hideOnNotTop(hide),
-      preventOverflow,
-      {
-        name: "offset",
-        options: {
-          offset: [0, 4],
-        },
-      },
-    ],
-  });
+  const [visible, setVisible] = createSignal(true);
 
   createEffect(() => {
     if (!visible()) return;
-    const popper = res();
-    if (!popper) return;
-    if (popper.state.placement.indexOf("top") !== 0 && visible()) {
-      setVisible(false);
-    }
-  });
-
-  createEffect(() => {
-    visible();
-    res()?.forceUpdate();
-    const t = setTimeout(() => res()?.forceUpdate(), 0);
-    onCleanup(() => clearTimeout(t));
-  });
-
-  createEffect(() => {
-    if (!visible()) return;
-    const d = popper();
-    if (!d) return;
-    const handle = (e: MouseEvent) => {
-      if (isClickOutside(e, d)) {
-        setVisible(false);
-      }
-    };
-    document.body.addEventListener("click", handle);
-    onCleanup(() => document.body.removeEventListener("click", handle));
+    const listener = () => setVisible(false);
+    window.addEventListener("scroll", listener);
+    onCleanup(() => {
+      window.removeEventListener("scroll", listener);
+    });
+    document.body.addEventListener("click", listener);
+    onCleanup(() => {
+      document.body.removeEventListener("click", listener);
+    });
   });
 
   return (
@@ -87,14 +38,23 @@ function PlusWithButton() {
         onClick={() => setVisible((visible) => !visible)}
       >
         +
-        <img ref={setAnchor} class={s.circle} src={circle} />
+        <img class={s.circle} src={circle} />
+        <Show when={visible()}>
+          <div class={s.popover}>
+            <div class={s.text}>
+              Со значком «+» указана минимальная стоимость
+            </div>
+            <div data-popper-arrow>
+              <img
+                width="20"
+                class={s.triangle}
+                src={triangle}
+                data-popper-arrow
+              />
+            </div>
+          </div>
+        </Show>
       </button>
-      <div ref={setPopper} class={s.popover} data-show={visible()}>
-        <div class={s.text}>Со значком «+» указана минимальная стоимость</div>
-        <div data-popper-arrow>
-          <img width="20" class={s.triangle} src={triangle} data-popper-arrow />
-        </div>
-      </div>
     </>
   );
 }
--- src/packages/isClickOutside.ts
+++
@@ -1,5 +0,0 @@
-
-export function isClickOutside(e: MouseEvent, el: HTMLElement): boolean {
-  return !el.contains(e.target as Node | null)
-}
-
