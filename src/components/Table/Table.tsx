98bcf903b2facb449d197d41e92427df34235437
whiteand <andrewbeletskiy@gmail.com>
2022-02-16 21:16:08 +0200

    added links to headers

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,8 +1,18 @@
-import { createMemo, For, onMount, Show } from "solid-js";
+import {
+  createEffect,
+  createMemo,
+  createSignal,
+  For,
+  onCleanup,
+  onMount,
+  Show,
+} from "solid-js";
 import { ColumnsHeaders } from "./ColumnsHeaders";
+import LinkSvg, * as T from "../../assets/link.svg";
 import { ITable } from "../../types/Table";
 import { Row } from "./Row";
 import s from "./Table.module.scss";
+import createClipboard from "@solid-primitives/clipboard";
 
 interface ITableProps
   extends Pick<ITable, "title" | "titleId" | "columns" | "data"> {
@@ -31,11 +41,43 @@ export default function Table(props: ITableProps) {
     );
   });
 
+  const [setClipboard] = createClipboard();
+  const [copied, setCopied] = createSignal(false);
+
+  const onHeaderLinkButtonClick = () => {
+    const url = new URL(`/`, window.location.origin);
+    url.hash = props.titleId;
+    window.history.replaceState(null, document.title, url.toString());
+    setClipboard(url.toString());
+    setCopied(false);
+    setCopied(true);
+  };
+
+  createEffect(() => {
+    if (!copied()) return;
+    const timeoutHandle = setTimeout(() => {
+      setCopied(false);
+    }, 500);
+    onCleanup(() => clearTimeout(timeoutHandle));
+  });
+
   return (
     <div ref={props.ref} class={s.wrapper}>
       <Show when={props.title}>
         <h3 class={s.header} id={props.titleId}>
-          {props.title}
+          <div class={s.headerText}>
+            <button
+              class={s.copyButton}
+              classList={{
+                [s.clicked]: copied(),
+              }}
+              type="button"
+              onClick={onHeaderLinkButtonClick}
+            >
+              <img width="18" height="18" src={LinkSvg} />
+            </button>
+            {props.title}
+          </div>
         </h3>
       </Show>
       <div class={s.table}>
